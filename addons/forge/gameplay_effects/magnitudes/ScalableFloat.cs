3f414f492de65d380c74b9f814f026fd6f405f09
Lex <lex@lextatic.com>
2025-04-06 00:08:45 -0300

    Added scalable float and scalable int magnitudes

---
+++ addons/forge/gameplay_effects/magnitudes/ScalableFloat.cs
@@ -0,0 +1,28 @@
+// Copyright © 2025 Gamesmiths Guild.
+
+using Gamesmiths.Forge.Core.Godot;
+using Godot;
+using ForgeScalableFloat = Gamesmiths.Forge.GameplayEffects.Magnitudes.ScalableFloat;
+
+namespace Gamesmiths.Forge.GameplayEffects.Magnitudes.Godot;
+
+[Tool]
+[GlobalClass]
+public partial class ScalableFloat : Resource
+{
+	[Export]
+	public float BaseValue { get; set; }
+
+	[Export]
+	public Curve ScalingCurve { get; set; }
+
+	public ScalableFloat(float baseValue)
+	{
+		BaseValue = baseValue;
+	}
+
+	public ForgeScalableFloat GetScalableFloat()
+	{
+		return new ForgeScalableFloat(BaseValue, new ForgeCurve(ScalingCurve));
+	}
+}
---
+++ addons/forge/gameplay_effects/magnitudes/ScalableInt.cs
@@ -0,0 +1,28 @@
+// Copyright © 2025 Gamesmiths Guild.
+
+using Gamesmiths.Forge.Core.Godot;
+using Godot;
+using ForgeScalableInt = Gamesmiths.Forge.GameplayEffects.Magnitudes.ScalableInt;
+
+namespace Gamesmiths.Forge.GameplayEffects.Magnitudes.Godot;
+
+[Tool]
+[GlobalClass]
+public partial class ScalableInt : Resource
+{
+	[Export]
+	public int BaseValue { get; set; }
+
+	[Export]
+	public Curve ScalingCurve { get; set; }
+
+	public ScalableInt(int baseValue)
+	{
+		BaseValue = baseValue;
+	}
+
+	public ForgeScalableInt GetScalableInt()
+	{
+		return new ForgeScalableInt(BaseValue, new ForgeCurve(ScalingCurve));
+	}
+}
